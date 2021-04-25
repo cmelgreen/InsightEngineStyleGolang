8eadaeae46bbb42ebca82349ae1c7d34d9828b46
cmelgreen <cmelgreen@gmail.com>
2021-04-24 22:13:34 -0500

    add toml file

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"text/template"
+
+	"github.com/mitchellh/mapstructure"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+const cssTemplate = `
+.text{
+	color: #{{.Color}};
+}
+`
+
+type cssParams struct {
+	Logo string
+	Color string
+}
+
+var t *template.Template
+
+func init() {
+	t = template.Must(template.New("css").Parse(cssTemplate))
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	params := cssParams{}
+	css := &bytes.Buffer{}
+
+	mapstructure.Decode(request.QueryStringParameters, &params)
+	t.Execute(css, params)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       css.String(),
+	}, nil
+}
